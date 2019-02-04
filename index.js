5f8ec15459b2896c69d84fd10b2c1c462a456378
Ray <raymondgiang@gmail.com>
2019-02-03 20:26:18 -0500

    omg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express"); 
 var http = require("http");
 var socketIO = require("socket.io")(server);
+socketIO.on('connection', () => { /* … */ });
 var path = require("path");
 
 var app = express();
